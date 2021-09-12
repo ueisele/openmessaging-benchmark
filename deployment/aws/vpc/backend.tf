1d4de6e2cc58b05ef669ea51393d0081d5ad82c1
Uwe Eisele <code@uweeisele.eu>
2021-09-12 21:26:41 +0200

    deployment/aws: vpc - s3 backend

--- deployment/aws/vpc/backend.tf
+++ deployment/aws/vpc/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-aws-uweeisele-dev"
-    key     = "benchmark-vpc"
-    region  = "eu-central-1"
+    region         = "eu-central-1"
+    bucket         = "terraform-aws-uweeisele-dev"
+    key            = "benchmark-vpc"
+    dynamodb_table = "terraform-aws-uweeisele-dev"
   }
 }
